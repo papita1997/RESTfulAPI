ee0ea3429b315f7eee90e95b9c78e990c90396db
pawan kumar <crazyforprogramming@gmail.com>
2021-04-30 14:09:12 -0700

    version 1.0

---
+++ src/test/java/com/pawan/RESTfulAPI/ResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawan.RESTfulAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
